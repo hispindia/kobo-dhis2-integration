0a08fe7bc05994bd8460974fcb1b9d454ca3e155
sourabhB <sourabh.bhardwaj@hispindia.org>
2025-01-07 07:59:27 +0530

    add constants for API credentials, URLs, and configurations

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 import os
 
@@ -9,18 +8,17 @@ if os.path.exists(auth_file):
 else:
     raise FileNotFoundError("Authentication file not found!")
 
-
+# Kobo API Details
 KOBO_BASE_URL = "https://kobo.humanitarianresponse.info/api/v2/assets"
 KOBO_USERNAME = auth_data.get("KOBO_USERNAME")
 KOBO_PASSWORD = auth_data.get("KOBO_PASSWORD")
 
-
+# DHIS2 API Details
 DHIS2_BASE_URL = "https://me.yesdigital.org/yesdigital/api"
 DHIS2_USERNAME = auth_data.get("DHIS2_USERNAME")
 DHIS2_PASSWORD = auth_data.get("DHIS2_PASSWORD")
 
-
+# Other constants
 FORM_ID = ""
 LOG_FILE = datetime.now().strftime("%Y-%m-%d") + "_integration.log"
-REGION_VALUE = ""
 PROGRAM_ID = "ONV44BbaETD"
